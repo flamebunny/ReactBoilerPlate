3095e3b2e05f61c2ceba901addb10d5c3e86ba09
Deepesh <dbudhia@gmail.com>
2018-08-19 20:29:12 +0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Movies from './components/movies/movies'
+
+import GetMoviesButton from './components/getMoviesButton/getMoviesButton'
+
+// TODO: Use fragments?
+export const App = () =>
+  <div>
+    <Movies />
+    <GetMoviesButton />
+  </div>
---
+++ src/actions/movies.js
@@ -0,0 +1,46 @@
+import { getMoviesFromApiAsync } from "../api";
+
+export const GET_MOVIES = 'GET_MOVIES'
+export function getMovies(movies) {
+  return {
+	  type:  GET_MOVIES,
+	  movies: [{title: 'Lord of The Rings'}, {title: 'The Matrix'}]
+  }
+}
+
+export const MOVIES_FETCH = 'MOVIES_FETCH'
+export function moviesFetch() {
+  return {
+    type: MOVIES_FETCH
+  }
+}
+
+export const MOVIES_FETCH_ASYNC = 'MOVIES_FETCH_ASYNC'
+export function moviesFetchAsync() {
+  return (dispatch, getStore) => {
+    if(getStore().fetching) {
+      return
+    }
+    dispatch(moviesFetch())
+
+    getMoviesFromApiAsync()
+      .then(movies => dispatch(moviesFetchSuccess({ movies })))
+      .catch(error => dispatch(moviesFetchFailed({ error })))
+  }
+}
+
+export const MOVIES_FETCH_SUCCESS = 'MOVIES_FETCH_SUCCESS'
+export function moviesFetchSuccess({ movies }) {
+  return {
+    type: MOVIES_FETCH_SUCCESS,
+    movies
+  }
+}
+
+export const MOVIES_FETCH_FAILED = 'MOVIES_FETCH_FAILED'
+export function moviesFetchFailed({ error }) {
+  return {
+    type: MOVIES_FETCH_FAILED,
+    errorMessage: error.message
+  }
+}
